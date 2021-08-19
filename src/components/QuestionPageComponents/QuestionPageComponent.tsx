1a839d5b808f2053e4d42e541905e49ae27de03d
Pratik Raut <pratikraut0000@gmail.com>
2021-08-19 19:26:16 +0530

    Question page basic modification

--- src/components/QuestionPageComponents/QuestionPageComponent.tsx
+++ src/components/QuestionPageComponents/QuestionPageComponent.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import './QuestionPageComponent.css';
-import { useEffect } from 'react';
+
+import LoadingIcon from '../../static/main_loading.gif';
+
+import { useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router';
+import { useHistory } from 'react-router-dom';
+import { Question } from '../../classes/Question';
 
 interface MatchParams {
     questionId: string,
@@ -11,18 +16,36 @@ interface Props extends RouteComponentProps<MatchParams> { }
 
 const QuestionPageComponent = (props: Props) => {
 
-    const componentDidMount = async () => {
-        const questionId = props.match.params.questionId;
-        //TODO: fetch question data using question id here...
-    }
+    const [question, setQuestion] = useState<Question>();
+    const history = useHistory();
 
     useEffect(() => {
-        componentDidMount();
-    }, []);
+        const questionId = Number(props.match.params.questionId);
+        if (isNaN(questionId)) {
+            history.push('/404');
+            return;
+        }
+        //TODO: fetch question data using question id here...
+        setTimeout(() => {
+            setQuestion(new Question(1, 'Jane is Running', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+                new Date(), 5, 1,
+                'https://i.picsum.photos/id/781/300/500.jpg?hmac=HyDr7W7aw9LRkzQ3eYgKrLjjO0gXFYF_0VY0oAxM1bE'));
+        }, 2000);
+    }, [props.match.params.questionId, history]);
+
+    const getQuestionPageView = (): JSX.Element => {
+        if (question === undefined) return <img src={LoadingIcon} alt="loading" style={{ width: '50px' }} />;
+        //TODO: complete this page content properly
+        return (
+            <div className="questionPageContent_container">
+                <h2>This is QuestionPageComponent</h2>
+            </div>
+        );
+    }
 
     return (
-        <div>
-            <h2>This is QuestionPageComponent</h2>
+        <div className="questionPage_container">
+            {getQuestionPageView()}
         </div>
     );
 }
