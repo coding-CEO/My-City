a66ab967dabae5f90ba4270e4a9d25493eca0ede
Pratik Raut <pratikraut0000@gmail.com>
2021-08-19 13:28:38 +0530

    View is saparated according to subdomain

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,25 @@
 import * as React from 'react';
 import './App.css';
 
+import AuthorityView from './AuthorityComponents/AuthorityView';
+import CitizenView from './CitizenComponents/CitizenView';
+
 const App = () => {
+
+  const isAuthority = (): boolean => {
+    const hostName = window.location.hostname;
+    const domains = hostName.split('.');
+    return ((domains.length > 1) && (domains[0] === 'gov'));
+  }
+
+  const getViewAccordingToUrl = (): JSX.Element => {
+    if (isAuthority()) return <AuthorityView />
+    return <CitizenView />;
+  }
+
   return (
     <div className="App">
+      {getViewAccordingToUrl()}
     </div>
   );
 }
